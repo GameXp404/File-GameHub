5c25b2e6d714870160b444751a99ccedebf35f3d
GameXp404 <khusus.gsc.net@gmail.com>
2026-05-03 10:15:17 +0700

    Add PSX (PlayStation 1) emulator demo via EmulatorJS WASM core

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // GameHub Service Worker
 // Cache-first untuk asset statis, network-first untuk HTML
-const CACHE_VERSION = 'gamehub-v13-circuit-bg';
+const CACHE_VERSION = 'gamehub-v14-psx-demo';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
