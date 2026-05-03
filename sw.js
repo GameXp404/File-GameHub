cecceb54a308caeff80e03448f550af94c9de384
GameXp404 <khusus.gsc.net@gmail.com>
2026-05-03 08:42:10 +0700

    Restructure: dedicated bgLayer div for water animation + !important on glass styles

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // GameHub Service Worker
 // Cache-first untuk asset statis, network-first untuk HTML
-const CACHE_VERSION = 'gamehub-v7-bg-override-fix';
+const CACHE_VERSION = 'gamehub-v8-bglayer';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
